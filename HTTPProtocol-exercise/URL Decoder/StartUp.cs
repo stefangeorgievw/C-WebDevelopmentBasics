fd1e164cee6d37b91bc9488e306c55e9ec3436bf
stefangeorgievw <stefangeorgiev30@gmail.com>
2018-09-20 16:02:02 +0300

    Commit http homework

---
+++ HTTPProtocol-exercise/URL Decoder/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace URL_Decoder
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var urlInput = Console.ReadLine();
+            var decodedUrl = WebUtility.UrlDecode(urlInput);
+            Console.WriteLine(decodedUrl);
+        }
+    }
+}
---
+++ HTTPProtocol-exercise/ValidateURL/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+
+namespace ValidateURL
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var urlInput = Console.ReadLine();
+            var decodedUrl = WebUtility.UrlDecode(urlInput);
+            try
+            {
+                Uri uri = new Uri(decodedUrl);
+
+                if (string.IsNullOrEmpty(uri.Scheme) || string.IsNullOrEmpty(uri.Host) || string.IsNullOrEmpty(uri.AbsolutePath) || uri.Port < 0)
+                {
+
+                    Console.WriteLine("Invalid URL");
+                }
+                else if (uri.Scheme == "http" && uri.Port == 80 || uri.Scheme == "https" && uri.Port == 443)
+                {
+                    Console.WriteLine($"Protocol: {uri.Scheme}");
+                    Console.WriteLine($"Host: {uri.Host}");
+                    Console.WriteLine($"Port: {uri.Port}");
+                    Console.WriteLine($"Path: {uri.AbsolutePath}");
+
+                    if (!string.IsNullOrEmpty(uri.Query))
+                    {
+                        Console.WriteLine($"Query: {uri.Query}");
+                    }
+                    if (!string.IsNullOrEmpty(uri.Fragment))
+                    {
+                        Console.WriteLine($"Fragment: {uri.Fragment}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid URL");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Invalid URL");
+            }
+           
+        }
+    }
+}
